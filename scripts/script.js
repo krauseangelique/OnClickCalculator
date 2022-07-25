cbb56d0c99e18c72645cb5211cd6caefafe65ac9
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-25 14:29:49 +0200

    fin calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,3 @@
-let numberValue1 = 0;
-let numberValue2 = 0;
-let resultSum = numberValue1 + numberValue2;
-console.log(resultSum);
-let operationValue = "";
 
-
-function myFirstOperation() {
-    
-    console.log("Réaction au click");
-}
-document.getElementsByClassName("number").innerHTML.addEvenlistener("click"); 
 
 
